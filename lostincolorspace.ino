9f2d37f7cfe11b4d1146b6be86c3da0409bf7af0
Ali Moore <alicrockett@gmail.com>
2020-11-21 14:11:10 -0800

    goal success test bug fixes

--- lostincolorspace.ino
+++ lostincolorspace.ino
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <inttypes.h>
 
 #define PULSE_LENGTH 2000
 #define INTAKE_FACE 0
@@ -26,17 +27,17 @@ const byte primaryDoseMap[] = { 47,79,143,255,143,79 };
 // BUTTON CLICKS
 typedef byte buttonClick;
 const buttonClick SINGLE = 0;
-const buttonClick DOUBLE = 1;
-const buttonClick MULTI  = 2;
-const buttonClick LONG   = 3;
-const buttonClick NONE   = 4;
+const buttonClick MULTI  = 1;
+const buttonClick LONG   = 2;
+const buttonClick NONE   = 3;
 buttonClick click = NONE;
 
 // MESSAGE TYPES
 typedef byte messageType;
-const messageType REQUEST     = 0;
-const messageType SEND_COLOR  = 1;
-const messageType SET_PRIMARY = 2;
+const messageType SET_PRIMARY = 0;
+const messageType SET_BLANK   = 1;
+const messageType REQUEST     = 2;
+const messageType SEND_COLOR  = 3;
 const messageType WIN         = 4;
 const messageType LOSE        = 5;
 const messageType NO_MESSAGE  = 6;
@@ -50,7 +51,9 @@ bool requestSent = true;
 typedef byte boardInitMsg;
 const boardInitMsg SET_GREEN          = 0;
 const boardInitMsg SET_BLUE           = 1;
-const boardInitMsg SET_BLANK          = 3;
+
+byte neighbors[2] = { 7, 7 }; // aka undefined
+byte neighborCount = 0;
 
 // TEST RESULTS
 typedef byte testResult;
@@ -61,6 +64,15 @@ const testResult NEED_BLUE    = 3;
 
 testResult loseCondition;
 unsigned long loseAnimationStart;
+
+byte rgb[CHANNELS];
+void rgbInit() {
+  for (byte i = 0; i < CHANNELS; i++) {
+    rgb[i] = 0;
+  }
+}
+
+byte undoBuffer[CHANNELS] = { 0, 0, 0 };
 
 
 // MESSAGE PASSING FUNCTIONS
@@ -88,14 +100,6 @@ messageType getMessageType( byte f ) {
   return msg[f][0];
 }
 
-byte rgb[CHANNELS];
-void rgbInit() {
-  for (int i = 0; i < CHANNELS; i++) {
-    rgb[i] = 0;
-  }
-}
-
-byte undoBuffer[] = { 0, 0, 0 };
 
 void primaryInit( byte p ) {
   currentState = PRIMARY;
@@ -118,21 +122,11 @@ void setChip ( byte r, byte g, byte b ) {
   rgb[B] = b;
 }
 
-void setPrimary( const byte* m, byte f ) {
-    byte setupMsg;
+void mixIn ( int32_t r, int32_t g, int32_t b ) {
 
-    if ( *m == SET_GREEN ) {
-      primaryInit( G );
-    } else if ( *m == SET_BLUE ) {
-      primaryInit( B );
-    }
-}
-
-void mixIn ( unsigned int r, unsigned int g, unsigned int b ) {
-
-  unsigned int r2 = r + (unsigned int)rgb[R];
-  unsigned int g2 = g + (unsigned int)rgb[G];
-  unsigned int b2 = b + (unsigned int)rgb[B];
+  int32_t r2 = r + (int32_t)rgb[R];
+  int32_t g2 = g + (int32_t)rgb[G];
+  int32_t b2 = b + (int32_t)rgb[B];
 
   auto big = biggest(r2, g2, b2);
 
@@ -152,16 +146,32 @@ void setup() {
 
 void loop() {
 
+  neighborCount = countNeighbors();
+  setValueSentOnAllFaces( neighborCount );
+
   FOREACH_FACE(f) {
     if ( isDatagramReadyOnFace(f) ) {
-        msgLength[f] = getDatagramLengthOnFace(f);
-        msg[f] = getDatagramOnFace(f);
+      msgLength[f] = getDatagramLengthOnFace(f);
+      msg[f] = getDatagramOnFace(f);
+  
+      messageType t = getMessageType(f);
+      const byte* m = getMessage(f);
+
+      if ( t == SET_PRIMARY ) {
+        if ( *m == SET_GREEN ) {
+          primaryInit( G );
+        } else if ( *m == SET_BLUE ) {
+          primaryInit( B );
+        }
+      } else if ( t == SET_BLANK ) {
+        setChip( 0, 0, 0 );
+      }
     }
   }
   
   if ( buttonSingleClicked() ) click = SINGLE;
   else if ( buttonDoubleClicked() ) setChip( 0, 0, 0 );
-  else if ( buttonMultiClicked() ) makePrimaryBank();
+  else if ( buttonMultiClicked() ) boardInit();
   else if ( buttonLongPressed() ) click = LONG;
   else click = NONE;
 
@@ -214,9 +224,7 @@ void chipLoop() {
     messageType t = getMessageType(f);
     const byte* m = getMessage(f);
 
-    if ( t == SET_PRIMARY ) {
-      setPrimary( m, f );
-    } else if ( t == REQUEST ) {
+    if ( t == REQUEST ) {
       sendMessage( SEND_COLOR, rgb, COLOR_MSG_LENGTH, f );
     } else if ( ( t == SEND_COLOR ) && ( f == INTAKE_FACE ) ) {
       mixIn( m[R], m[G], m[B] );
@@ -224,6 +232,7 @@ void chipLoop() {
       currentState = WINNER;
     } else if ( t == LOSE ) {
       currentState = LOSER;
+      loseCondition = *m;
       loseAnimationStart = millis();
     }
   }
@@ -232,18 +241,6 @@ void chipLoop() {
   switch ( click ) {
     case SINGLE: 
       setChip( undoBuffer[R], undoBuffer[G], undoBuffer[B] );
-      break;
-    case DOUBLE:
-      if ( isAlone() ) {
-        rgbInit();
-      } else {
-        makePrimaryBank();
-      }
-      break;
-    case MULTI:
-      if ( isAlone() ) {
-        randChipInit();
-      }
       break;
     case LONG:
       currentState = GOAL;
@@ -269,10 +266,7 @@ void primaryLoop() {
     rgb[currentPrimary] = primaryDoseMap[f];
 
     messageType t = getMessageType(f);
-    if ( t == SET_PRIMARY ) {
-      const byte* m = getMessage(f);
-      setPrimary( m, f );
-    }
+
     if ( t == REQUEST ) {
       sendMessage( SEND_COLOR, rgb, COLOR_MSG_LENGTH, f );
     }
@@ -281,9 +275,6 @@ void primaryLoop() {
   }
 
   // CLICK HANDLING
-  if ( click == DOUBLE ) {
-      randChipInit();
-  }
   if ( click == SINGLE ) {
     cyclePrimary();
   }
@@ -313,10 +304,6 @@ void goalLoop() {
   if ( click == LONG ) {
     currentState = CHIP;
   }
-  if ( click == DOUBLE ) {
-    randChipInit();
-    currentState = GOAL;
-  }
 
   // DISPLAY
   FOREACH_FACE(f) {
@@ -327,8 +314,8 @@ void goalLoop() {
 }
 
 void winnerLoop() {
-  if ( isAlone() ) {
-    setChip( rgb[R], rgb[G], rgb[B] );
+  if ( isValueReceivedOnFaceExpired( INTAKE_FACE ) ) {
+    currentState = CHIP;
   }
 
   FOREACH_FACE(f) {
@@ -344,10 +331,10 @@ void winnerLoop() {
 
 void loserLoop() {
 
-  if ( isAlone() ) {
-    setChip( rgb[R], rgb[G], rgb[B] );
+  if ( isValueReceivedOnFaceExpired( INTAKE_FACE ) ) {
+    currentState = CHIP;
   }
- 
+   
   unsigned long animationElapsed = millis() - loseAnimationStart; 
   if ( animationElapsed > 1000 ) {
     FOREACH_FACE(f) {
@@ -372,16 +359,16 @@ void pulse( Color col, byte f ) {
 }
 
 testResult checkColor( byte r, byte g, byte b ) {
-  int rdiff = ((int)rgb[R]) - r;
-  int gdiff = ((int)rgb[G]) - g;
-  int bdiff = ((int)rgb[B]) - b;
+  int32_t rdiff = ((int32_t)rgb[R]) - r;
+  int32_t gdiff = ((int32_t)rgb[G]) - g;
+  int32_t bdiff = ((int32_t)rgb[B]) - b;
 
   //float colorDistance  = sqrt( pow( rdiff, 2 ) + pow( gdiff, 2 ) + pow( bdiff, 2 ) ); 
-  unsigned int colorDistance = (rdiff * rdiff) + (gdiff * gdiff) + (bdiff * bdiff);
+  int32_t colorDistance = (rdiff * rdiff) + (gdiff * gdiff) + (bdiff * bdiff);
   testResult result;
-  
-  if ( colorDistance < 8000 ) {
-    return CLOSE_ENOUGH;
+
+  if ( ( colorDistance < 8000 ) ) { 
+    result = CLOSE_ENOUGH;
   } else {
     if ( ( gdiff > rdiff) && ( gdiff > bdiff ) ) {
       result = NEED_GREEN;
@@ -390,34 +377,82 @@ testResult checkColor( byte r, byte g, byte b ) {
     } else {
       result = NEED_RED;
     }
-
-    loseCondition = result;
-    return result;
   }
+  
+  return result;
+
+}
+
+byte countNeighbors() {
+  byte count = 0;
+  FOREACH_FACE(f) {
+    if ( !isValueReceivedOnFaceExpired(f) ) count++;
+  }
+  return count;
+}
+
+void find2neighbors() {
+  if ( neighborCount > 2 ) {
+    return;
+  } else {
+    byte whichNeighbor = 0;
+
+    FOREACH_FACE(f) { 
+      if ( !isValueReceivedOnFaceExpired(f) ) {
+        neighbors[whichNeighbor] = f;
+        whichNeighbor++; 
+      }
+    }
+  }
+}
+
+void boardInit() {
+  
+  find2neighbors();
+ 
+  if ( isAlone() ) {
+    randChipInit();
+    currentState = GOAL;
+  } else if ( isDuo() ) {
+    makeChipPrimaryPair();
+  } else if ( isTriangle() ) {
+    makePrimaryBank();
+  } else {
+    // FIX set all blank
+  }
+
+}
+
+bool isDuo() {
+  if ( ( neighborCount == 1 ) && 
+       ( getLastValueReceivedOnFace(neighbors[0]) == 1) ) { 
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bool isTriangle() {
+  if ( ( neighborCount == 2 ) &&
+       ( getLastValueReceivedOnFace( neighbors[0] ) == 2) &&   
+       ( getLastValueReceivedOnFace( neighbors[1] ) == 2) &&   
+       ( ( ( neighbors[1] - neighbors[0] ) % 4 ) == 1 )
+     ) 
+  { return true; } else { return false; }
+}
+
+void makeChipPrimaryPair() {
 }
 
 void makePrimaryBank() {
   primaryInit( R );
-  
-  byte neighborCount = 0;
-  byte neighbors[2] = { 7, 7 }; // aka undefined
-  FOREACH_FACE(f) { 
-    if ( !isValueReceivedOnFaceExpired(f) ) {
-      if ( neighborCount < 2 ) {
-        neighbors[neighborCount] = f;
-        neighborCount++; 
-      } else {
-        break;
-      }
-    }
-  }
 
-  if ( neighborCount == 2 ) {
-    byte setupMsg = SET_GREEN;
-    sendMessage( SET_PRIMARY, &setupMsg, 1, neighbors[0] );
-    setupMsg = SET_BLUE;
-    sendMessage( SET_PRIMARY, &setupMsg, 1, neighbors[1] );
-  }
+  find2neighbors(); 
+
+  byte setupMsg = SET_GREEN;
+  sendMessage( SET_PRIMARY, &setupMsg, 1, neighbors[0] );
+  setupMsg = SET_BLUE;
+  sendMessage( SET_PRIMARY, &setupMsg, 1, neighbors[1] );
 }
 
 void cyclePrimary() {
@@ -425,8 +460,8 @@ void cyclePrimary() {
   primaryInit(currentPrimary);
 }
 
-unsigned int biggest ( unsigned int r, unsigned int g, unsigned int b ) {
-  unsigned int result = r;
+int32_t biggest ( int32_t r, int32_t g, int32_t b ) {
+  int32_t result = r;
   if ( g > result ) result = g;
   if ( b > result ) result = b;
   return result;
